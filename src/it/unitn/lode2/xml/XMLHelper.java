6a835b5e3e61e4d8faba758435a8b986f5fc6f55
Tiziano Lattisi <tiziano@axiastudio.it>
2015-02-02 08:46:44 +0100

    Un helper per le operazioni di marshall e unmarshall.

---
+++ src/it/unitn/lode2/xml/XMLHelper.java
@@ -0,0 +1,42 @@
+package it.unitn.lode2.xml;
+
+import it.unitn.lode2.xml.timedslides.TimedSlides;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.Writer;
+import java.util.Optional;
+
+/**
+ * User: tiziano
+ * Date: 27/01/15
+ * Time: 15:40
+ */
+public class XMLHelper {
+
+    public static Marshaller createMarshaller(Class klass){
+        try {
+            JAXBContext context = JAXBContext.newInstance(klass);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            return marshaller;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Unmarshaller createUnmarshaller(Class klass){
+        try {
+            JAXBContext context = JAXBContext.newInstance(klass);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            return unmarshaller;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
