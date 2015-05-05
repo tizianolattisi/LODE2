bed7366c002ceeac1130731dbed29ee7a6221e8d
Tiziano Lattisi <tiziano.lattisi@gmail.com>
2015-05-05 14:10:27 +0200

    Una dialog in partenza propone di avviare la sessione di registrazione per l'ultima lezione dell'ultimo corso usato.

--- src/main/java/it/unitn/lode2/Main.java
+++ src/main/java/it/unitn/lode2/Main.java
@@ -1,32 +1,34 @@
 package it.unitn.lode2;
 
+import it.unitn.lode2.asset.Lecture;
+import it.unitn.lode2.asset.xml.XmlCourseImpl;
+import it.unitn.lode2.ui.controllers.WizardController;
 import it.unitn.lode2.xml.XMLHelper;
 import it.unitn.lode2.xml.prefs.XMLLodePrefs;
 import it.unitn.lode2.xml.prefs.XMLProperty;
 import it.unitn.lode2.xml.prefs.XMLSection;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class Main extends Application {
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
-        // From file dialog
-        /*
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extension = new FileChooser.ExtensionFilter("Lecture's configuration file (LECTURE.XML)","*.xml");
-        fileChooser.getExtensionFilters().add(extension);
-        File file = fileChooser.showOpenDialog(null);
-        String lectureFileName = file.toURI().toURL().getFile();
-        String lectureFolder = lectureFileName.substring(0, lectureFileName.lastIndexOf("/"));
-        String courseFolder = lectureFolder.substring(0, lectureFolder.lastIndexOf("Acquisition"));
-        */
-
-        // Last used course
+        // There's a lecture to record?
         String courseFolder = null;
         XMLLodePrefs prefs = XMLHelper.build(XMLLodePrefs.class).unmarshal(new File(Constants.LODE_PREFS));
         for( XMLSection section: prefs.getSections() ){
@@ -40,7 +42,46 @@ public class Main extends Application {
                 break;
             }
         }
-        RecordingSessionLaucher.launch(new Stage(), courseFolder);
+        if( courseFolder!=null ){
+            XmlCourseImpl course = new XmlCourseImpl(courseFolder);
+            Lecture lecture = course.lectures().get(course.lectures().size() - 1);
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Lecture ready for the recording session.");
+            String msg = "Do you want to start the recording session for the lecture:\n\n \""
+                       + lecture.name()
+                       + "\" ("
+                       + course.name()
+                       + ").";
+            alert.setHeaderText(msg);
+            //alert.setContentText("Choose your option.");
+            ButtonType buttonTypeRecord = new ButtonType("Record");
+            ButtonType buttonTypeWizard = new ButtonType("Go to wizard");
+            ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+            alert.getButtonTypes().setAll(buttonTypeRecord, buttonTypeWizard, buttonTypeCancel);
+            Optional<ButtonType> result = alert.showAndWait();
+            if( result.isPresent() ){
+                if( result.get()==buttonTypeRecord) {
+                    RecordingSessionLaucher.launch(new Stage(), course.getFolderPath());
+                } else if( result.get()==buttonTypeWizard ){
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/wizard.fxml"));
+                    Parent root = null;
+                    try {
+                        root = loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    WizardController controller = loader.getController();
+                    controller.setLodeCoursesPath(Constants.LODE_COURSES);
+                    primaryStage.setTitle("LODE2 Wizard");
+                    Scene scene = new Scene(root, 500, 345);
+
+                    primaryStage.setScene(scene);
+                    //primaryStage.setOnCloseRequest(controller.handlerClose);
+                    primaryStage.show();
+                }
+
+            }
+        }
 
     }
 
