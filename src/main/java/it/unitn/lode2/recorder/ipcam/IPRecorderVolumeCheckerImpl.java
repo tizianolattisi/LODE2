1534219b7c60e3ac61b68e0697f1e65312b69947
Tiziano Lattisi <tiziano@axiastudio.it>
2016-03-03 12:27:21 +0100

    Modifica all'implementazione del volume checker, che ora non estende più Thread, ma ha in pancia un Thread.

--- src/main/java/it/unitn/lode2/recorder/ipcam/IPRecorderVolumeCheckerImpl.java
+++ src/main/java/it/unitn/lode2/recorder/ipcam/IPRecorderVolumeCheckerImpl.java
@@ -15,11 +15,51 @@ import java.io.InputStreamReader;
  * Date: 09/03/15
  * Time: 11:03
  */
-public class IPRecorderVolumeCheckerImpl extends Thread implements VolumeChecker {
+public class IPRecorderVolumeCheckerImpl implements VolumeChecker {
 
+    class VolumeCheckerThread extends Thread {
+        @Override
+        public void run() {
+            assert Platform.isFxApplicationThread()==false;
+            try {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader br = new BufferedReader(isr);
+                String line = null;
+                while( (line = br.readLine()) != null ) {
+                    // [Parsed_ebur128_0 @ 0x7fe36bc1a140] t: 52.0867    M: -66.6 S: -63.5     I: -24.7 LUFS     LRA:  21.0 LU
+                    if( line.startsWith("[Parsed_ebur128") ){
+                        try {
+                            int start = line.indexOf("M:") + 2;
+                            int end = line.indexOf("S:", start);
+                            String substring = line.substring(start, end);
+                            Double m = Double.parseDouble(substring); // NumberFormatException
+                            Platform.runLater(() -> lufs.setValue(m));
+                        } catch (StringIndexOutOfBoundsException ex) {
+                            logger.error("Unable to parse ebur128 line.");
+                        }
+                        //System.out.println(line);
+                    } else if( line.startsWith("frame=") ) {
+                        // do noting
+                    } else if( line.startsWith("[mpegts") ) {
+                        // do noting
+                    } else {
+                        logger.debug(line);
+                    }
+                    //final String fLine = line;
+                    //Platform.runLater(() -> System.out.println(fLine+"\n"));
+                }
+                inputStream = null;
+                logger.debug("Stream gobbler terminated.");
+            }
+            catch (IOException ioe) {
+                logger.error("Unable to read from ffmpeg output.");
+            }
+        }
+    }
+
+    VolumeCheckerThread thread;
     InputStream inputStream;
     DoubleProperty lufs;
-    Boolean toTerminate = Boolean.FALSE;
 
     final static Logger logger = Logger.getLogger(IPRecorderVolumeCheckerImpl.class);
 
@@ -38,45 +78,15 @@ public class IPRecorderVolumeCheckerImpl extends Thread implements VolumeChecker
     }
 
     @Override
-    public void run() {
-        assert Platform.isFxApplicationThread()==false;
-        try {
-            InputStreamReader isr = new InputStreamReader(inputStream);
-            BufferedReader br = new BufferedReader(isr);
-            String line = null;
-            while( !toTerminate && (line = br.readLine()) != null ) {
-                // [Parsed_ebur128_0 @ 0x7fe36bc1a140] t: 52.0867    M: -66.6 S: -63.5     I: -24.7 LUFS     LRA:  21.0 LU
-                if( line.startsWith("[Parsed_ebur128") ){
-                    try {
-                        int start = line.indexOf("M:") + 2;
-                        int end = line.indexOf("S:", start);
-                        String substring = line.substring(start, end);
-                        Double m = Double.parseDouble(substring); // NumberFormatException
-                        Platform.runLater(() -> lufs.setValue(m));
-                    } catch (StringIndexOutOfBoundsException ex) {
-                        logger.error("Unable to parse ebur128 line.");
-                    }
-                    //System.out.println(line);
-                } else if( line.startsWith("frame=") ) {
-                    // do noting
-                } else if( line.startsWith("[mpegts") ) {
-                    // do noting
-                } else {
-                    logger.debug(line);
-                }
-                //final String fLine = line;
-                //Platform.runLater(() -> System.out.println(fLine+"\n"));
-            }
-            logger.debug("Stream gobbler terminated.");
-        }
-        catch (IOException ioe) {
-            logger.error("Unable to read from ffmpeg output.");
-        }
+    public void start() {
+        new VolumeCheckerThread().start();
     }
+
 
     @Override
     public void terminate() {
-        toTerminate = Boolean.TRUE;
+        //toTerminate = Boolean.TRUE;
+        //inputStream = null;
     }
 
 }
